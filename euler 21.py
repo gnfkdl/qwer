d462a7df0ae5c343ca3459a63af52d903e82c412
gnfkdl <zcadqe132@naver.com>
2023-12-28 05:48:38 +0900

    project euler

--- euler 21.py
+++ euler 21.py
@@ -1,19 +1,14 @@
-sum_cache = {}
 equal_divisor = 0
 
 def num_divisor(num):    
     cache_1 = 0
     for i in range(1,round(num/2)+1):
         if num % i == 0: cache_1 += i
-    sum_cache[num] = cache_1
 
     if num == cache_1: return 0
     return cache_1
 
-
 for num in range(2,10001):
     if num == num_divisor(num_divisor(num)): equal_divisor += num
 
-    
-print(sum_cache)
 print(equal_divisor)
---
+++ euler 26.py
@@ -0,0 +1,25 @@
+longest_float, longest_denominator = 0
+
+def fraction(denominator):
+    menurator = 1
+    digit_of_floats = []
+
+    while menurator != 0:
+        if menurator in digit_of_floats:    return len(digit_of_floats) - digit_of_floats.index(menurator)
+
+        digit_of_floats.append(menurator)
+        menurator *= 10
+
+        while menurator < denominator: 
+            digit_of_floats.append(0)
+            menurator *= 10
+
+        menurator %= denominator
+    return 0
+
+
+for denominator in range(1,1001):
+    cache = fraction(denominator)
+    if longest_float < cache:   longest_float, longest_denominator = cache, denominator 
+
+print('가장 큰 순환을 가지는 분수 값:',longest_denominator)
