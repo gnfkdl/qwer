54f72ae8de92e0a2f54b19d3e1eb9cc288aabc4d
gnfkdl <153505143+gnfkdl@users.noreply.github.com>
2023-12-28 01:53:08 +0900

    project euler

--- euler 22.py
+++ euler 22.py
@@ -1,7 +1,7 @@
 answer = 0
 
 with open("0022_names.txt", 'r') as file:
-    names = (file.read()).lower()
+    names = (file.read()).lower() 
     names = sorted((names.replace('"','')).split(','))
     
 alphabet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
