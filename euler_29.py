55544eb9f1fb3a1cbca076f809009d876a88edc0
gnfkdl <zcadqe132@naver.com>
2024-01-13 02:24:13 +0900

    project_euler

---
+++ euler_29.py
@@ -0,0 +1,10 @@
+def powers(i,j):
+    if i**j in cache: return False
+    cache[(i**j)] = i**j
+    return True
+
+cache = {}
+answer = [i*j for i in range(2,101) for j in range(2,101) if powers(i,j)]
+
+print(len(answer))
+        
